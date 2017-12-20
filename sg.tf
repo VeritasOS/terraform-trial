c05de3ed434087907ceeb9343872e9714ed5e6cf
Dan LaMotte <daniel.lamotte@veritas.com>
2017-12-20 14:20:49 -0600

    finish up security groups

--- sg.tf
+++ sg.tf
@@ -4,3 +4,82 @@ resource "aws_default_security_group" "default" {
   # adopt default security group and drop all ingress/egress rules as per AWS
   # security best practices
 }
+
+resource "aws_security_group" "standard" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  name        = "standard"
+  description = "Standard network policy"
+}
+
+resource "aws_security_group_rule" "standard_ingress_ssh" {
+  security_group_id = "${aws_security_group.standard.id}"
+
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = "${var.internal_networks}"
+}
+
+resource "aws_security_group_rule" "standard_ingress_http" {
+  security_group_id = "${aws_security_group.standard.id}"
+
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = "${var.internal_networks}"
+}
+
+resource "aws_security_group_rule" "standard_ingress_https" {
+  security_group_id = "${aws_security_group.standard.id}"
+
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = "${var.internal_networks}"
+}
+
+resource "aws_security_group_rule" "standard_ingress_rdp" {
+  security_group_id = "${aws_security_group.standard.id}"
+
+  type        = "ingress"
+  from_port   = 3389
+  to_port     = 3389
+  protocol    = "tcp"
+  cidr_blocks = "${var.internal_networks}"
+}
+
+# TODO this policy ensures that hosts cannot egress to internet even through
+# the corporate network, do we really need that level of security?
+resource "aws_security_group_rule" "standard_egress_ssh" {
+  security_group_id = "${aws_security_group.standard.id}"
+
+  type        = "egress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = "${var.internal_networks}"
+}
+
+resource "aws_security_group_rule" "standard_egress_http" {
+  security_group_id = "${aws_security_group.standard.id}"
+
+  type        = "egress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = "${var.internal_networks}"
+}
+
+resource "aws_security_group_rule" "standard_egress_https" {
+  security_group_id = "${aws_security_group.standard.id}"
+
+  type        = "egress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = "${var.internal_networks}"
+}
--- vars.tf
+++ vars.tf
@@ -1,3 +1,14 @@
+variable "internal_networks" {
+  description = "List of internal network cidrs"
+  type        = "list"
+
+  default = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+  ]
+}
+
 variable "name" {
   description = "A name for this"
   type        = "string"
