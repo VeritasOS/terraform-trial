fff8f785b48cff38e87a30ca25f38f0a910dd2cc
Dan LaMotte <daniel.lamotte@veritas.com>
2017-12-20 11:00:23 -0600

    adopt default vpc/sg

---
+++ sg.tf
@@ -0,0 +1,6 @@
+resource "aws_default_security_group" "default" {
+  vpc_id = "${aws_default_vpc.default.id}"
+
+  # adopt default security group and drop all ingress/egress rules as per AWS
+  # security best practices
+}
